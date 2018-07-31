d6a311b841bcc730269f36c872bbee0913a7c455
winlin <winlin@vip.126.com>
2018-07-31 14:23:24 +0800

    Nodejs appserver

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # rtc-app-nodejs
+
 Nodejs AppServer for RTC.
+
+For all languages:
+
+* [Python](https://github.com/winlinvip/rtc-app-python).
+* [Java](https://github.com/winlinvip/rtc-app-java).
+* [Golang](https://github.com/winlinvip/rtc-app-golang).
+* [PHP](https://github.com/winlinvip/rtc-app-php).
+* [C#](https://github.com/winlinvip/rtc-app-csharp).
+* [Nodejs](https://github.com/winlinvip/rtc-app-nodejs).
+
+For RTC deverloper:
+
+* RTC [workflow](https://help.aliyun.com/document_detail/74889.html).
+* RTC [token generation](https://help.aliyun.com/document_detail/74890.html).
+
+## Usage
+
+1. Generate AK from [here](https://usercenter.console.aliyun.com/#/manage/ak):
+
+```
+AccessKeyID: OGAEkdiL62AkwSgs
+AccessKeySecret: 4JaIs4SG4dLwPsQSwGAHzeOQKxO6iw
+```
+
+2. Create APP from [here](https://rtc.console.aliyun.com/#/manage):
+
+```
+AppID: iwo5l81k
+```
+
+3. Clone project and generate config:
+
+```
+git clone https://github.com/winlinvip/rtc-app-nodejs.git &&
+cd rtc-app-nodejs && npm install &&
+echo "module.exports = {" > config.js &&
+echo "  listen: 8080," >> config.js &&
+echo "  appId: 'iwo5l81k'," >> config.js &&
+echo "  accessKeyId: 'OGAEkdiL62AkwSgs'," >> config.js &&
+echo "  accessKeySecret: '4JaIs4SG4dLwPsQSwGAHzeOQKxO6iw'," >> config.js &&
+echo "  endpoint: 'https://rtc.aliyuncs.com'," >> config.js &&
+echo "  gslb: 'https://rgslb.rtc.aliyuncs.com'" >> config.js &&
+echo "};" >> config.js &&
+node index.js
+```
+
+4. Verify AppServer by [here](http://localhost:8080/app/v1/login?room=5678&user=nvivy&passwd=12345678).
+
+> Remark: You can setup client native SDK by `http://30.2.228.19:8080/app/v1`.
+
+> Remark: Please use your AppServer IP instead by `ifconfig eth0`.
+
---
+++ index.js
@@ -0,0 +1,116 @@
+var config = require('./config');
+console.log('Server listen=' + config.listen + ', appid=' + config.appId
+	+ ', akId=' + config.accessKeyId + ', akSecret=' + config.accessKeySecret
+	+ ', gslb=' + config.gslb + ', region=' + config.endpoint);
+
+const http = require('http');
+
+var channels = {};
+
+const url = require('url');
+const query = require('querystring');
+const uuidv4 = require('uuid/v4');
+
+const server = http.createServer((req, res) => {
+	if (req.headers['Origin'] != '') {
+			res.setHeader("Access-Control-Allow-Origin", "*");
+			res.setHeader("Access-Control-Allow-Methods", "GET,POST,HEAD,PUT,DELETE,OPTIONS");
+			res.setHeader("Access-Control-Expose-Headers", "Server,Range,Content-Length,Content-Range");
+			res.setHeader("Access-Control-Allow-Headers", "Origin,Range,Accept-Encoding,Referer,Cache-Control,X-Proxy-Authorization,X-Requested-With,Content-Type");
+	}
+
+	if (req.method == "OPTIONS") {
+		res.end();
+		return;
+	}
+
+	var q = query.parse(url.parse(req.url).query);
+	var channelId = q['room'];
+	var user = q['user'];
+	var channelUrl = config.appId + '/' + channelId;
+	console.log('Request channelId=' + channelId + ', user=' + user + ', appid=' + config.appId);
+
+	Promise.resolve().then(()=>{
+		var auth = channels[channelId];
+		if (auth) {
+			return auth;
+		}
+
+		return CreateChannel(config.appId, channelId, config.accessKeyId, config.accessKeySecret, config.endpoint).then((auth) => {
+			channels[channelUrl] = auth;
+			console.log('Create requestId=' + auth.requestId + ', channelId=' + channelId
+				+ ', nonce=' + auth.nonce + ', timestamp=' + auth.timestamp + ', channelKey='
+				+ auth.channelKey);
+			return auth;
+		});
+	}).then((auth) => {
+		var userId = uuidv4();
+		var session = uuidv4();
+		return CreateToken(channelId, auth.channelKey, config.appId, userId, session, auth.nonce, auth.timestamp).then((token) => {
+			return [auth, userId, session, token];
+		});
+	}).then(([auth, userId, session, token]) => {
+		var username = userId + '?appid=' + auth.appId + '&session=' + session
+			+ '&channel=' + auth.channelId + '&nonce=' + auth.nonce
+			+ '&timestamp=' + auth.timestamp;
+		console.log('Sign user=' + userId + ', session=' + session + ', token=' + token);
+
+		res.setHeader("Content-Type", "application/json");
+		res.end(JSON.stringify({
+			code: 0,
+			data: {
+				appid: auth.appId,
+				userid: userId,
+				gslb: [config.gslb],
+				session: session,
+				token: token,
+				nonce: auth.nonce,
+				timestamp: auth.timestamp,
+				turn: {
+					username: username,
+					password: token
+				}
+			}
+		}));
+	}).catch((error) => {
+		console.error(error);
+		res.writeHead(500);
+		res.end(error.message);
+	});
+});
+
+server.listen(config.listen);
+
+const sha256 = require('sha256/lib/sha256');
+function CreateToken(channelId, channelKey, appId, userId, session, nonce, timestamp) {
+	return new Promise((resolve, reject) => {
+		var token = sha256(channelId + channelKey + appId + userId + session + nonce + timestamp);
+		resolve(token);
+	});
+}
+
+var RTCClient = require('@alicloud/rtc-2018-01-11/lib/client');
+function CreateChannel(appId, channelId, accessKeyId, accessKeySecret, endpoint) {
+	return new Promise((resolve, reject) => {
+		var rtc = new RTCClient({
+			accessKeyId: accessKeyId,
+			accessKeySecret: accessKeySecret,
+			endpoint: endpoint
+		});
+
+		rtc.createChannel({
+			AppId: appId, ChannelId: channelId
+		}).then((res) => {
+			resolve({
+				appId: appId,
+				channelId: channelId,
+				requestId: res.RequestId,
+				nonce: res.Nonce,
+				timestamp: res.Timestamp,
+				channelKey: res.ChannelKey
+			});
+		}).catch((error) => {
+			reject(error);
+		});
+	});
+}
